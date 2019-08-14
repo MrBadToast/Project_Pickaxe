21d172307f5ccc5e6e18b16a406a2054f8257550
BadToast <ship2042@gmail.com>
2019-08-15 00:26:00 +0900

    Revert "Work_0813"
    This reverts commit e045737fc0e4218167045a217eb9479da899827b.

--- Assets/Scripts/MapSwitcher.cs
+++ Assets/Scripts/MapSwitcher.cs
@@ -9,9 +9,6 @@ public class MapSwitcher : MonoBehaviour
     [Space]
     public MapInfo Map2;
     public Transform Map2_EnterPos;
-    [Space]
-    public bool keepY = false;
-    public bool keepX = false;
 
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -28,25 +25,17 @@ public class MapSwitcher : MonoBehaviour
         {
             Map2.MapObject.SetActive(true);
             MainVirtualCamera.Instance.SetConfinerColider(Map2.camColider);
-            if (keepY == true)
-                collision.transform.position = new Vector2(Map2_EnterPos.position.x, collision.transform.position.y);
-            else if( keepX == true)
-                collision.transform.position = new Vector2(collision.transform.position.x, Map2_EnterPos.position.y);
-            else
-                collision.transform.position = Map2_EnterPos.position;
+            collision.transform.position = Map2_EnterPos.position;
             Map1.MapObject.SetActive(false);
         }
         else if (Map2.MapObject.activeInHierarchy)
         {
             Map1.MapObject.SetActive(true);
             MainVirtualCamera.Instance.SetConfinerColider(Map1.camColider);
-            if (keepY == true)
-                collision.transform.position = new Vector2(Map1_EnterPos.position.x, collision.transform.position.y);
-            else if (keepX == true)
-                collision.transform.position = new Vector2(collision.transform.position.x, Map1_EnterPos.position.y);
-            else
-                collision.transform.position = Map1_EnterPos.position;
+            collision.transform.position = Map1_EnterPos.position;
             Map2.MapObject.SetActive(false);
         }
+
+        
     }
 }
